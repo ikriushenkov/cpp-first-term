fe2706ffe15d76f5fcd93ed6443220ba89d93fa2
Ilya <kriushenkovi@gmail.com>
2020-07-05 21:21:32 +0300

    added bigintv1.0

--- bigint/big_integer.h
+++ bigint/big_integer.h
@@ -2,14 +2,15 @@
 #define BIG_INTEGER_H
 
 #include <cstddef>
-#include <gmp.h>
+#include <vector>
 #include <iosfwd>
+#include <cstdint>
 
-struct big_integer
-{
+struct big_integer {
     big_integer();
     big_integer(big_integer const& other);
     big_integer(int a);
+    big_integer(uint32_t a);
     explicit big_integer(std::string const& str);
     ~big_integer();
 
@@ -48,7 +49,20 @@ struct big_integer
     friend std::string to_string(big_integer const& a);
 
 private:
-    mpz_t mpz;
+    bool sign;
+    std::vector <uint32_t> data_;
+
+    uint32_t trial(big_integer &d, size_t k, size_t m);
+    bool smaller(big_integer &dq, size_t k, size_t m);
+    void difference(big_integer &dq, size_t k, size_t m);
+    big_integer& div_by_short(uint32_t a);
+    void remove_zeros();
+
+    static big_integer abs(big_integer a);
+
+    friend big_integer bit_operation(big_integer a, big_integer b, uint32_t func(uint32_t, uint32_t));
+
+    void to_addition_two();
 };
 
 big_integer operator+(big_integer a, big_integer const& b);
@@ -60,6 +74,7 @@ big_integer operator%(big_integer a, big_integer const& b);
 big_integer operator&(big_integer a, big_integer const& b);
 big_integer operator|(big_integer a, big_integer const& b);
 big_integer operator^(big_integer a, big_integer const& b);
+
 
 big_integer operator<<(big_integer a, int b);
 big_integer operator>>(big_integer a, int b);
